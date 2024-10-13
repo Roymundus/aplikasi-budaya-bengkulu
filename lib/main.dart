8a532a28196ede69718029271b053efc271f127a
Roymundus <90660998+Roymundus@users.noreply.github.com>
2024-10-13 12:34:14 +0800

    baikin tampilan

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:budaya_bengkulu/pages/hasil_test.dart';
-import 'package:budaya_bengkulu/pages/test_pages.dart';
+import 'package:budaya_bengkulu/pages/homepage.dart';
 import 'package:flutter/material.dart';
-import 'package:budaya_bengkulu/pages/homekuis.dart';
 
 void main() {
   runApp(const MyApp());
