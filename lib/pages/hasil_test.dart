06dbed9aa975ebff7d002d3337d344219af35277
Roymundus <90660998+Roymundus@users.noreply.github.com>
2024-10-13 12:34:36 +0800

    baikin tampilan

--- lib/pages/hasil_test.dart
+++ lib/pages/hasil_test.dart
@@ -15,22 +15,62 @@ class _HasilPageState extends State<HasilPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
+      appBar: AppBar(
+        title: const Text("Hasil Kuis"),
+        backgroundColor: Colors.green, // Sesuaikan warna sesuai keinginan
+      ),
+      body: Stack(
+        children: [
+          // Gambar latar belakang
+          Positioned.fill(
+            child: Opacity(
+              opacity: 1.0, // Transparansi gambar latar belakang
+              child: Image.asset(
+                'assets/backgroundHomePage.png', // Pastikan gambar ada di folder assets
+                fit: BoxFit.cover, // Agar gambar menutupi seluruh layar
+              ),
+            ),
+          ),
+          SafeArea(
             child: Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-          Image.asset(
-            'assets/best-seller.png',
-            width: 150,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/best-seller.png',
+                    width: 150,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Selamat!! Jawaban Kamu Benar sebanyak " +
+                        widget.result.toString(),
+                    style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black), // Ubah warna teks agar kontras
+                  ),
+                  const SizedBox(height: 20), // Ruang tambahan sebelum tombol
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.popUntil(context, (route) => route.isFirst);
+                    },
+                    child: const Text("Kembali ke Menu Utama"),
+                    style: ElevatedButton.styleFrom(
+                      primary: Color.fromARGB(255, 22, 119, 26), // Warna tombol
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 15),
+                      textStyle: const TextStyle(
+                          fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            "Selamat!! Jawaban Kamu Benar sebanyak " + widget.result.toString(),
-            style: TextStyle(fontSize: 15),
-          )
-        ]))));
+        ],
+      ),
+    );
   }
 }
--- lib/pages/homekuis.dart
+++ lib/pages/homekuis.dart
@@ -1,25 +1,29 @@
 import 'dart:convert';
-
 import 'package:budaya_bengkulu/models/question_model.dart';
 import 'package:budaya_bengkulu/pages/test_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as myhttp;
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class HomeKuis extends StatefulWidget {
+  const HomeKuis({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<HomeKuis> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomeKuis> {
   late QuestionModel questionsModel;
   TextEditingController usernameController = TextEditingController();
+  bool isLoading = false;
 
   final String url =
       "https://script.google.com/macros/s/AKfycbxMo7SLLOK_5vDGzWzdC3Vqwy-4cQMZELDzrlMXMqGTp55VVhy7Yv4JzwyxaospgMBbRg/exec";
 
   void getAllData(String username) async {
+    setState(() {
+      isLoading = true;
+    });
+
     try {
       var response = await myhttp.get(Uri.parse(url));
 
@@ -32,43 +36,92 @@ class _HomePageState extends State<HomePage> {
               )));
     } catch (err) {
       print("Error : " + err.toString());
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
     }
   }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.greenAccent,
-      body: SafeArea(
-        child: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            const Text(
-              "Silahkan Mengikuti Kuis",
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextFormField(
-                controller: usernameController,
-                decoration: const InputDecoration(
-                    hintText: "Silahkan masukan nama anda",
-                    fillColor: Colors.white,
-                    filled: true),
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Aksi untuk tombol kembali
+          },
+        ),
+        title: Align(
+          alignment: Alignment.centerLeft, // Mengatur agar judul rata kiri
+          child: const Text('Kuis Ensiklopedia Budaya Bengkulu'),
+        ),
+      ),
+      body: Stack(
+        children: [
+          // Gambar latar belakang
+          Positioned.fill(
+            child: Opacity(
+              opacity: 1.0, // Transparansi gambar
+              child: Image.asset(
+                'assets/backgroundHomePage.png', // Pastikan gambar ada di folder assets
+                fit: BoxFit.cover,
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                getAllData(usernameController.text);
-              },
-              child: Text("Mulai Kuis"),
-            )
-          ]),
-        ),
+          ),
+          SafeArea(
+            child: Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Silahkan Mengikuti Kuis",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextFormField(
+                        controller: usernameController,
+                        decoration: const InputDecoration(
+                            hintText: "Silahkan masukan nama anda",
+                            fillColor: Colors.white,
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                            )),
+                      ),
+                    ),
+                    isLoading
+                        ? const CircularProgressIndicator()
+                        : ElevatedButton(
+                            onPressed: () {
+                              getAllData(usernameController.text);
+                            },
+                            child: const Text("Mulai Kuis"),
+                            style: ElevatedButton.styleFrom(
+                              primary: Color.fromARGB(
+                                  255, 22, 119, 26), // Warna tombol
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 30, vertical: 15),
+                              textStyle: const TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                  ]),
+            ),
+          ),
+        ],
       ),
     );
   }
