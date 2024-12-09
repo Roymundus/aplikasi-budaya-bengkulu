0035036261c55829b67b2669789b61c4083faa53
Roymundus <90660998+Roymundus@users.noreply.github.com>
2024-12-09 20:18:20 +0800

    fix adat istiadat

--- lib/pages/adat_istiadat.dart
+++ lib/pages/adat_istiadat.dart
@@ -1,113 +1,238 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: deprecated_member_use
 
-class AdatIstiadatPage extends StatelessWidget {
-  final List<AdatDetailModel> adatDetails = [
-    AdatDetailModel(
-      'Upacara Adat',
-      'Upacara adat adalah salah satu bentuk tradisi yang dilakukan oleh masyarakat untuk memperingati peristiwa penting, seperti kelahiran, pernikahan, dan kematian.',
-      'assets/upacara-adat.jpg',
-    ),
-    AdatDetailModel(
-      'Tradisi',
-      'Tradisi adalah kebiasaan yang dilakukan secara turun-temurun oleh masyarakat, seperti tradisi mandi safar di Bengkulu.',
-      'assets/tradisi.jpg',
-    ),
-    AdatDetailModel(
-      'Ritual',
-      'Ritual adalah kegiatan keagamaan atau budaya yang dilakukan dengan tata cara khusus, seperti ritual Tabot di Bengkulu.',
-      'assets/ritual.jpeg',
-    ),
-    AdatDetailModel(
-      'Perayaan',
-      'Perayaan adalah kegiatan yang dilakukan untuk merayakan suatu peristiwa penting dalam budaya, seperti perayaan Tabot di Bengkulu.',
-      'assets/perayaan.jpeg',
-    ),
-  ];
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:budaya_bengkulu/pages/pilih_kabupaten.dart';
+
+class AdatIstiadatPage extends StatefulWidget {
+  final KabupatenModel kabupaten;
+
+  const AdatIstiadatPage({
+    Key? key,
+    required this.kabupaten,
+  }) : super(key: key);
+
+  @override
+  _AdatIstiadatPageState createState() => _AdatIstiadatPageState();
+}
+
+class _AdatIstiadatPageState extends State<AdatIstiadatPage> {
+  final DatabaseReference database = FirebaseDatabase.instance.ref();
+  List<Map<String, dynamic>> adatList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchadatIstiadat();
+  }
+
+  void fetchadatIstiadat() async {
+    try {
+      final snapshot = await database
+          .child("-OCW6dIqvmXRFCCware0/kabupaten/1/budaya/adat_istiadat")
+          .get();
+
+      if (snapshot.value != null) {
+        final data = snapshot.value as Map<dynamic, dynamic>;
+
+        setState(() {
+          adatList = data.entries.map((entry) {
+            final adat = entry.value as Map<dynamic, dynamic>;
+            return {
+              "nama": adat["nama"] ?? "Nama tidak tersedia.",
+              "deskripsi": adat["deskripsi"] ?? "Deskripsi tidak tersedia.",
+              "gambar": adat["gambar"] ??
+                  "assets/367ba491fe4460d44becee0e83083c2b.jpg",
+            };
+          }).toList();
+        });
+      } else {
+        setState(() {
+          adatList = [];
+        });
+      }
+    } catch (e) {
+      setState(() {
+        adatList = [];
+      });
+    }
+  }
+
+  // Fungsi untuk menampilkan popup detail
+  void _showDetailPopup(Map<String, dynamic> adat) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          contentPadding: const EdgeInsets.all(16.0),
+          title: Text(
+            adat["nama"],
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.green,
+            ),
+          ),
+          content: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Gambar Besar
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: InteractiveViewer(
+                    child: Image.network(
+                      adat["gambar"],
+                      fit: BoxFit.contain,
+                      errorBuilder: (context, error, stackTrace) {
+                        return const Icon(
+                          Icons.broken_image,
+                          size: 200,
+                          color: Colors.grey,
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                // Deskripsi Lengkap
+                Text(
+                  adat["deskripsi"],
+                  style: const TextStyle(
+                    fontSize: 18,
+                    color: Colors.black87,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Tutup'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Adat Istiadat"),
-        backgroundColor: Colors.green, // Sesuaikan dengan tema aplikasi
+        title: Text("Adat Istiadat"),
+        backgroundColor: Colors.green,
       ),
       body: Container(
-        color: Colors.green, // Latar belakang aplikasi menjadi hijau
-        child: ListView.builder(
-          padding: const EdgeInsets.all(16.0),
-          itemCount: adatDetails.length,
-          itemBuilder: (context, index) {
-            return AdatDetailItem(adatDetail: adatDetails[index]);
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class AdatDetailModel {
-  final String title;
-  final String description;
-  final String imagePath;
-
-  AdatDetailModel(this.title, this.description, this.imagePath);
-}
-
-class AdatDetailItem extends StatelessWidget {
-  final AdatDetailModel adatDetail;
-
-  const AdatDetailItem({Key? key, required this.adatDetail}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        // Aksi ketika item diklik, bisa diarahkan ke halaman detail adat istiadat
-      },
-      child: Card(
-        elevation: 5,
-        margin: const EdgeInsets.symmetric(vertical: 10),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-        color: Colors.white, // Ubah warna latar belakang Card menjadi putih
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Row(
-            children: [
-              Image.asset(
-                adatDetail.imagePath,
-                width: 80,
-                height: 80,
-                fit: BoxFit.cover,
-              ),
-              const SizedBox(width: 16),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      adatDetail.title,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors
-                            .black, // Ubah warna teks menjadi hitam agar kontras
+        color: Colors.white,
+        padding: const EdgeInsets.all(16.0),
+        child: adatList.isNotEmpty
+            ? ListView.builder(
+                itemCount: adatList.length,
+                itemBuilder: (context, index) {
+                  final adat = adatList[index];
+                  return Card(
+                    margin: const EdgeInsets.only(bottom: 16.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    elevation: 5,
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: GestureDetector(
+                        onTap: () => _showDetailPopup(adat),
+                        child: Row(
+                          children: [
+                            // Gambar kecil
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(12),
+                              child: Image.network(
+                                adat["gambar"],
+                                width: 120,
+                                height: 120,
+                                fit: BoxFit.cover,
+                                errorBuilder: (context, error, stackTrace) {
+                                  return const Icon(
+                                    Icons.broken_image,
+                                    size: 120,
+                                    color: Colors.grey,
+                                  );
+                                },
+                              ),
+                            ),
+                            const SizedBox(width: 16),
+                            // Nama dan Deskripsi
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  // Nama Senjata
+                                  Text(
+                                    adat["nama"],
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.green,
+                                    ),
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  const SizedBox(height: 8),
+                                  Text(
+                                    adat["deskripsi"],
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.black87,
+                                    ),
+                                    maxLines: 3,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                    const SizedBox(height: 8),
-                    Text(
-                      adatDetail.description,
-                      style: const TextStyle(
-                        fontSize: 14,
-                        color: Colors
-                            .black54, // Ubah warna teks deskripsi menjadi hitam sedikit transparan
+                  );
+                },
+              )
+            : Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.search_off,
+                      size: 100,
+                      color: Colors.grey,
+                    ),
+                    const SizedBox(height: 16),
+                    const Text(
+                      "Data Adat Istiadat tidak tersedia.",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black54,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 16),
+                    ElevatedButton.icon(
+                      onPressed: fetchadatIstiadat,
+                      icon: const Icon(Icons.refresh),
+                      label: const Text("Coba Lagi"),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                        onPrimary: Colors.white,
                       ),
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
       ),
     );
   }
